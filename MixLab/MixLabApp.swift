9ecda0670b5e29143881f68f6ba26cf431f5eca0
WPITCHFORK <60607062+WPITCHFORK@users.noreply.github.com>
2023-03-12 16:33:28 +0000

    Initial Commit

---
+++ MixLab/MixLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixLabApp.swift
+//  MixLab
+//
+//  Created by Will Pitchfork on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MixLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
