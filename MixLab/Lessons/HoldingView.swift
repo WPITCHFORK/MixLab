c6be8c3de00b974d2b2a05ac62b9600bc79875f9
WPITCHFORK <60607062+WPITCHFORK@users.noreply.github.com>
2023-03-30 10:51:31 +0100

    Working buttons now

--- MixLab/Lessons/HoldingView.swift
+++ MixLab/Lessons/HoldingView.swift
@@ -12,6 +12,7 @@ struct HoldingView: View {
     @State var lessonDestination: any View = NewView()
     var loading: Bool = true
     
+    
     var body: some View {
         NavigationStack {
             ZStack {
@@ -53,49 +54,61 @@ struct HoldingView: View {
                         
                         Spacer()
                         
-                        Button (action: {
-                            switch (selectedLesson.id) {
-                                case 1:
-                                    print("Go to lesson 1")
-                                case 2:
-                                    print("Go to lesson 2")
-                                case 3:
-                                    print("Go to lesson 3")
-                                case 4:
-                                    print("Go to lesson 4")
-                                case 5:
-                                    print("Go to lesson 5")
-                                case 6:
-                                    print("Go to lesson 6")
-                                case 7:
-                                    print("Go to lesson 7")
-                                case 8:
-                                    print("Go to lesson 8")
-                                case 9:
-                                    print("Go to lesson 9")
-                                case 10:
-                                    print("Go to lesson 10")
-                                case 11:
-                                    print("Go to lesson 11")
-                                case 12:
-                                    print("Go to lesson 12")
-                                case 13:
-                                    print("Go to lesson 13")
-                                case 14:
-                                    print("Go to lesson 14")
-                                default:
-                                    print("I don't know what lesson you're actually wanting to load here... you've broken it again 🙄")
+                        if selectedLesson.id == 1 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 1")) { GoButton() }
                             }
-                        }) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 50)
-                                    .frame(width: 150, height: 40)
-                                    .foregroundColor(CustomBlue)
-                                
-                                Text("Start Lesson")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(CustomLightBlue)
-                                
+                        } else if selectedLesson.id == 2 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 2")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 3 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 3")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 4 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 4")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 5 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 5")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 6 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 6")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 7 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 7")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 8 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 8")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 9 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 9")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 10 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 10")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 11 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 11")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 12 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 12")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 13 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 13")) { GoButton() }
+                            }
+                        } else if selectedLesson.id == 14 {
+                            Button (action: { }) {
+                                NavigationLink(destination: Text("Lesson 14")) { GoButton() }
                             }
                         }
                         
@@ -130,9 +143,23 @@ extension View where Self == ActivityIndicator {
     }
 }
 
-struct HoldingView_Previews: PreviewProvider {
-    static var previews: some View {
-        HoldingView(selectedLesson: lessonInfo(id: 1, ShortTitle: "Intro", LongTitle: "Introduction to Music Technology", ImageName: "magnifyingglass", ImageSize: 150, Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
-            .previewInterfaceOrientation(.landscapeLeft)
+//struct HoldingView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HoldingView(selectedLesson: lessonInfo(id: 1, ShortTitle: "Intro", LongTitle: "Introduction to Music Technology", ImageName: "magnifyingglass", ImageSize: 150, Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."), lessons: <#[lessonInfo]#>)
+//            .previewInterfaceOrientation(.landscapeLeft)
+//    }
+//}
+
+struct GoButton: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 50)
+                .frame(width: 150, height: 40)
+                .foregroundColor(CustomBlue)
+            
+            Text("Start Lesson")
+                .fontWeight(.bold)
+                .foregroundColor(CustomLightBlue)
+        }
     }
 }
